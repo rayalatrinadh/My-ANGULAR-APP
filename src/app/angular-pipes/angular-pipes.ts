67cde3aa90a2b8896a8b9b25a6ce5228489bf12e
Trinadh Rayala <3nadhmail@gmail.com>
2026-01-18 10:43:40 -0600

    angluar pipe changes

---
+++ src/app/angular-pipes/angular-pipes.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-pipes',
+  standalone:true,
+  imports: [],
+  templateUrl: './angular-pipes.html',
+  styleUrl: './angular-pipes.css',
+})
+export class AngularPipes {
+
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {UpperCasePipe, LowerCasePipe,CurrencyPipe, DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UpperCasePipe, 
+    LowerCasePipe,
+    CurrencyPipe,
+    DatePipe,
+   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-angular-app');
+
+  nameTitle : string = 'trinadh rayala';
+
+  amountToDisplay : number = 777.757;
+
+  todayDate : Date = new Date();
+
+  trinadhUser : any = {
+    name : 'Trinadh Rayala',
+    age : 26,
+    email : '3nadhmail@gmail.com'
+  }
 }
